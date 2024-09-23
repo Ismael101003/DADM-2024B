b0a31f31b4d05c5403dadbf516b3639e3eb2a308
Ismael101003 <80792425+Ismael101003@users.noreply.github.com>
2024-09-23 18:51:56 +0000

    ⭐ FEAT: add label an id in the list

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <script setup>
 import { ref } from 'vue'
 const header = ref('App lista de compras')
+const items = ref([
+{ id: '0', label: 'bolillos'}, 
+ {id: '1', label: 'Preentreno'}, 
+ { id: '2', label: '1 Nutella'}])
 </script>
 
 <template>
   <H1>
     <i class="material-icons shopping-cart-icon"> local_mall</i>
     {{ header }}
+    <ul>
+      <li v-for="item in items" :key="item">📬 {{ item.label }}</li>
+    </ul>
   </H1>
 </template>
 

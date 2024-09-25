311579dc78cdf427906d9456f1f90399b11e65fa
Ismael101003 <80792425+Ismael101003@users.noreply.github.com>
2024-09-25 18:40:29 +0000

    ⭐Feat: Add the radio button

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,22 @@ const items = ref([
   { id: '1', label: 'Preentreno' },
   { id: '2', label: '1 Nutella' }
 ])
-const newItem = ref()
+const newItem = ref('');
+const newitemPriority = ref('low'); 
 </script>
 
 <template>
   <H1>
     <i class="material-icons shopping-cart-icon"> local_mall</i>
     {{ header }}
+  </H1>
     <ul>
       <li v-for="item in items" :key="item">📬 {{ item.label }}</li>
-    </ul>
-  </H1>
-  <input v-model="newItem" type="text" placeholder="Agregar producto" /> {{ newItem }}
+    <input v-model="newItem" type="text" placeholder="Agregar producto">
+    <label><input type="radio" value="low" v-model="newitemPriority"> baja</label>
+    <label><input type="radio" value="hight" v-model="newitemPriority"> Alta</label>    
+  </ul>
+    
 </template>
 
 <style scoped>
